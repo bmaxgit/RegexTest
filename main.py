b71a259d0a54ee0b12e3a2c33606a29cf5ae2d28
bw <bw@localhost>
2017-07-16 11:53:07 +0200

    Frame arrangement implemented

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class MyFirstGUI:
         self.master = master
         master.title("Title")
 
-
+        # Input frame
         self.inputFrame=Frame(master)
         self.inputFrame.pack(side=TOP)
 
@@ -17,15 +17,44 @@ class MyFirstGUI:
         self.inputText.configure(yscrollcommand=self.scroll.set)
         self.scroll.pack(side=RIGHT, fill=Y)
 
-        self.label = Label(master, text="This is our first GUI!")
-        self.label.pack(side=TOP)
+        # Regex frame
+        self.regexFrame=Frame(master)
+        self.regexFrame.pack()
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack(side=BOTTOM)
+        # Regex flag frame
+        self.regexFlagFrame=Frame(self.regexFrame)
+        self.regexFlagFrame.pack()
+
+        # Output frame
+        self.outputFrame=Frame(master)
+        self.outputFrame.pack()
+
+        # Group frame
+        self.outputGroupFrame=Frame(self.outputFrame)
+        self.outputGroupFrame.pack()
+
+        # Button frame
+        self.buttonFrame=Frame(master)
+        self.buttonFrame.pack()
+
+
+
+        # self.xlabel = []
+        # for i in range(5):
+        #     self.xlabel.append(Label(self.inputFrame, text=str(i)))
+        #     self.xlabel[i].pack()
+        #
+        # self.label = Label(master, text="This is our first GUI!")
+        # self.label.pack(side=TOP)
+
+
+
+        self.greet_button = Button(self.buttonFrame, text="Greet", command=self.greet)
+        self.greet_button.pack(side=LEFT)
         self.greet_button.pack()
 
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack(side=BOTTOM)
+        self.close_button = Button(self.buttonFrame, text="Close", command=master.quit)
+        self.close_button.pack(side=RIGHT)
         self.close_button.pack()
 
     def greet(self):
