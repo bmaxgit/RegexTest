f1bb20635ec910e9ffbba96d5a31f40d3d21f240
bw <bw@localhost>
2017-07-16 12:32:40 +0200

    Regex widgets added

--- main.py
+++ main.py
@@ -9,32 +9,50 @@ class MyFirstGUI:
         self.inputFrame=Frame(master)
         self.inputFrame.pack(side=TOP)
 
-        self.inputText = Text(self.inputFrame, height=20, width=80)
+        self.inputText = Text(self.inputFrame, height=10, width=80)
         self.inputText.insert(END, "Insert test text")
         self.inputText.pack(side=LEFT)
 
-        self.scroll = Scrollbar(self.inputFrame, command=self.inputText.yview)
-        self.inputText.configure(yscrollcommand=self.scroll.set)
-        self.scroll.pack(side=RIGHT, fill=Y)
+        self.inputScroll = Scrollbar(self.inputFrame, command=self.inputText.yview)
+        self.inputText.configure(yscrollcommand=self.inputScroll.set)
+        self.inputScroll.pack(side=RIGHT, fill=Y)
 
         # Regex frame
-        self.regexFrame=Frame(master)
+        self.regexFrame = Frame(master)
         self.regexFrame.pack()
 
+        self.regexText = Text(self.regexFrame, height=3, width=80)
+        self.regexText.insert(END, "")
+        self.regexText.pack(side=LEFT)
+
+        self.regexScroll = Scrollbar(self.regexFrame, command=self.regexText.yview)
+        self.regexText.configure(yscrollcommand=self.regexScroll.set)
+        self.regexScroll.pack(side=RIGHT, fill=Y)
+
         # Regex flag frame
-        self.regexFlagFrame=Frame(self.regexFrame)
+        self.regexFlagFrame = Frame(master)
         self.regexFlagFrame.pack()
 
+        flagMultilineVar = IntVar(value=False)
+
+        self.flagMultiline = Checkbutton(self.regexFlagFrame, text="Multiline", var=flagMultilineVar)
+        self.flagMultiline.pack(side=RIGHT)
+
+        flagNoCaseVar = IntVar(value=False)
+
+        self.flagNoCase = Checkbutton(self.regexFlagFrame, text="NoCase", var=flagNoCaseVar)
+        self.flagNoCase.pack(side=RIGHT)
+
         # Output frame
-        self.outputFrame=Frame(master)
+        self.outputFrame = Frame(master)
         self.outputFrame.pack()
 
         # Group frame
-        self.outputGroupFrame=Frame(self.outputFrame)
+        self.outputGroupFrame = Frame(self.outputFrame)
         self.outputGroupFrame.pack()
 
         # Button frame
-        self.buttonFrame=Frame(master)
+        self.buttonFrame = Frame(master)
         self.buttonFrame.pack()
 
 
