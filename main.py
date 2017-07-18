984d5aaafb73bbf0ce62e62a0782e8527692ee4f
bw <bw@localhost>
2017-07-18 19:42:54 +0200

    Added comments, variables conforming to PEP8

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from Tkinter import *
 import re
 
+
 class RegexTestGUI:
     def __init__(self, master):
         self.master = master
         master.title("RegexTest")
 
         # Input frame
-        self.inputFrame=Frame(master)
+        self.inputFrame = Frame(master)
         self.inputFrame.pack(side=TOP)
 
+        # Input text
         self.inputText = Text(self.inputFrame, height=10, width=80)
         self.inputText.insert(END, "Insert test text")
         self.inputText.pack(side=LEFT)
 
+        # Input scrollbar
         self.inputScroll = Scrollbar(self.inputFrame, command=self.inputText.yview)
         self.inputText.configure(yscrollcommand=self.inputScroll.set)
         self.inputScroll.pack(side=RIGHT, fill=Y)
@@ -22,26 +25,31 @@ class RegexTestGUI:
         self.regexFrame = Frame(master)
         self.regexFrame.pack()
 
+        # Regex text
         self.regexText = Text(self.regexFrame, height=3, width=80)
         self.regexText.insert(END, "")
         self.regexText.pack(side=LEFT)
 
+        # Regex scrollbar
         self.regexScroll = Scrollbar(self.regexFrame, command=self.regexText.yview)
         self.regexText.configure(yscrollcommand=self.regexScroll.set)
         self.regexScroll.pack(side=RIGHT, fill=Y)
 
-        # Regex flag frame
+        # Flag frame
         self.regexFlagFrame = Frame(master)
         self.regexFlagFrame.pack()
 
+        # Flag multiline
         self.flagMultilineVar = BooleanVar(value=False)
         self.flagMultiline = Checkbutton(self.regexFlagFrame, text="Multiline", var=self.flagMultilineVar)
         self.flagMultiline.pack(side=RIGHT)
 
+        # Flag ignorecase
         self.flagIgnoreCaseVar = BooleanVar(value=False)
         self.flagIgnoreCase = Checkbutton(self.regexFlagFrame, text="IgnoreCase", var=self.flagIgnoreCaseVar)
         self.flagIgnoreCase.pack(side=RIGHT)
 
+        # Flag dotall
         self.flagDotAllVar = BooleanVar(value=False)
         self.flagDotAll = Checkbutton(self.regexFlagFrame, text="DotAll", var=self.flagDotAllVar)
         self.flagDotAll.pack(side=RIGHT)
@@ -50,10 +58,12 @@ class RegexTestGUI:
         self.outputFrame = Frame(master)
         self.outputFrame.pack()
 
+        # Output text
         self.outputText = Text(self.outputFrame, height=10, width=80)
         self.outputText.insert(END, "")
         self.outputText.pack(side=LEFT)
 
+        # Output scrollbar
         self.outputScroll = Scrollbar(self.outputFrame, command=self.outputText.yview)
         self.outputText.configure(yscrollcommand=self.outputScroll.set)
         self.outputScroll.pack(side=RIGHT, fill=Y)
@@ -61,6 +71,7 @@ class RegexTestGUI:
         # Group frame
         self.outputGroupFrame = Frame(master)
 
+        # Group checkboxes
         self.groupButton = []
         self.outputGroupButton = []
         for i in range(9):
@@ -73,10 +84,12 @@ class RegexTestGUI:
         self.buttonFrame = Frame(master)
         self.buttonFrame.pack()
 
+        # Execute frame
         self.executeButton = Button(self.buttonFrame, text="Execute", command=self.execute)
         self.executeButton.pack(side=LEFT)
         self.executeButton.pack()
 
+        # Exit frame
         self.exitButton = Button(self.buttonFrame, text="Close", command=master.quit)
         self.exitButton.pack(side=RIGHT)
         self.exitButton.pack()
@@ -85,25 +98,25 @@ class RegexTestGUI:
         print("Executing!")
 
         print "Input is [", self.inputText.get('1.0', 'end-1c'), "]"
-        inputText = self.inputText.get('1.0', 'end-1c')
+        input_text = self.inputText.get('1.0', 'end-1c')
 
         print "Regex is [", self.regexText.get('1.0', 'end-1c'), "]"
-        regexText = self.regexText.get('1.0', 'end-1c')
+        regex_text = self.regexText.get('1.0', 'end-1c')
 
         print "Output is [", self.outputText.get('1.0', 'end-1c'), "]"
 
         print "Multi is ", self.flagMultilineVar.get()
         print "IgnoreCase is ", self.flagIgnoreCaseVar.get()
         print "DotAll is ", self.flagDotAllVar.get()
-        regexFlag = 0
+        regex_flag = 0
         if self.flagMultilineVar.get() is True:
-            regexFlag |= re.MULTILINE
+            regex_flag |= re.MULTILINE
         if self.flagIgnoreCaseVar.get() is True:
-            regexFlag |= re.IGNORECASE
+            regex_flag |= re.IGNORECASE
         if self.flagDotAllVar.get() is True:
-            regexFlag |= re.DOTALL
+            regex_flag |= re.DOTALL
 
-        result = re.match(regexText, inputText, regexFlag)
+        result = re.match(regex_text, input_text, regex_flag)
         if result:
             print "Match!"
             self.outputText.delete(1.0, END)
@@ -117,8 +130,6 @@ class RegexTestGUI:
                     self.outputText.insert(END, ": [")
                     self.outputText.insert(END, result.group(i+1))
                     self.outputText.insert(END, "]\n")
-
-
 
 
 root = Tk()
