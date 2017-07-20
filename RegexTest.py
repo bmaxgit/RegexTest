9ab72f28bfb023a64c64b7a90f99927a680ae106
bmaxgit <bmaxwolf+git@gmail.com>
2017-07-20 22:19:25 +0200

    Changed class RegexTestGUI to function

--- RegexTest.py
+++ RegexTest.py
@@ -2,157 +2,174 @@ from Tkinter import *
 import re
 
 
-class RegexTestGUI:
-    def __init__(self, master):
-        self.master = master
-        master.title("RegexTest")
+def execute():
 
-        def button1callback(event):
-            print 'callback event', event
-            self.execute()
+    input_text = inputText.get('1.0', 'end-1c')
+    regex_text = regexText.get('1.0', 'end-1c')
 
-        self.master.bind('<Button-1>', button1callback)
-        self.master.bind('<Key>', button1callback)
+    regex_flag = 0
+    if flagMultilineVar.get() is True:
+        regex_flag |= re.MULTILINE
+    if flagIgnoreCaseVar.get() is True:
+        regex_flag |= re.IGNORECASE
+    if flagDotAllVar.get() is True:
+        regex_flag |= re.DOTALL
+    if flagUnicodeVar.get() is True:
+        regex_flag |= re.UNICODE
 
-        # Input frame
-        self.inputFrame = Frame(master)
-        self.inputFrame.pack(side=TOP, fill=BOTH, expand=YES)
+    try:
+        result = re.search(regex_text, input_text, regex_flag)
+        outputText.delete(1.0, END)
+        if result:
+            outputText.insert(END, result.group(0))
+            outputText.insert(END, "\n")
 
-        # Input text
-        self.inputText = Text(self.inputFrame, height=5, width=60)
-        self.inputText.insert(END, "Insert test text")
-        self.inputText.pack(side=LEFT, fill=BOTH, expand=YES)
+            for group_index in range(9):
+                try:
+                    group_text = result.group(group_index+1)
+                    outputGroupCheckbox[group_index].configure(state='normal')
+                    if groupCheckboxVar[group_index].get():
+                        outputText.insert(END, group_index+1)
+                        outputText.insert(END, ": [")
+                        outputText.insert(END, group_text)
+                        outputText.insert(END, "]\n")
 
-        # Input scrollbar
-        self.inputScroll = Scrollbar(self.inputFrame, command=self.inputText.yview)
-        self.inputText.configure(yscrollcommand=self.inputScroll.set)
-        self.inputScroll.pack(side=RIGHT, fill=Y)
+                        # Colorize group match
+                        start_index = '1.'+str(result.start(group_index+1))
+                        end_index = '1.'+str(result.end(group_index + 1))
+                        if group_index % 2 is 0:
+                            outputText.tag_add('group0', start_index, end_index)
+                            outputText.tag_config('group0', background='orange', foreground='red')
+                        else:
+                            outputText.tag_add('group1', start_index, end_index)
+                            outputText.tag_config('group1', background='yellow', foreground='red')
 
-        # Regex frame
-        self.regexFrame = Frame(master)
-        self.regexFrame.pack(fill=X)
+                except IndexError:
+                    outputGroupCheckbox[group_index].configure(state='disabled')
+                    groupCheckboxVar[group_index].set(False)
+        else:
+            for group_index in range(9):
+                outputGroupCheckbox[group_index].configure(state='disabled')
 
-        # Regex text
-        self.regexText = Text(self.regexFrame, height=3, width=60)
-        self.regexText.insert(END, "")
-        self.regexText.pack(side=LEFT, fill=X, expand=YES)
-
-        # Regex scrollbar
-        self.regexScroll = Scrollbar(self.regexFrame, command=self.regexText.yview)
-        self.regexText.configure(yscrollcommand=self.regexScroll.set)
-        self.regexScroll.pack(side=RIGHT, fill=Y)
-
-        # Flag frame
-        self.regexFlagFrame = Frame(master)
-        self.regexFlagFrame.pack(anchor=W)
-
-        # Flags
-        self.flagMultiline, self.flagMultilineVar = self.getcheckbox(self.regexFlagFrame, "Multiline")
-        self.flagIgnoreCase, self.flagIgnoreCaseVar = self.getcheckbox(self.regexFlagFrame, "IgnoreCase")
-        self.flagDotAll, self.flagDotAllVar = self.getcheckbox(self.regexFlagFrame, "DotAll")
-        self.flagUnicode, self.flagUnicodeVar = self.getcheckbox(self.regexFlagFrame, "Unicode")
-
-        # Output frame
-        self.outputFrame = Frame(master)
-        self.outputFrame.pack(fill=BOTH, expand=YES)
-
-        # Output text
-        self.outputText = Text(self.outputFrame, height=5, width=60)
-        self.outputText.insert(END, "")
-        self.outputText.pack(side=LEFT, fill=BOTH, expand=YES)
-        self.outputText.bind("<Key>", lambda e: "break")
-
-        # Output scrollbar
-        self.outputScroll = Scrollbar(self.outputFrame, command=self.outputText.yview)
-        self.outputText.configure(yscrollcommand=self.outputScroll.set)
-        self.outputScroll.pack(side=RIGHT, fill=Y)
-
-        # Group frame
-        self.outputGroupFrame = Frame(master)
-        self.outputGroupFrame.pack(anchor=W)
-
-        # Group checkboxes
-        self.groupCheckboxVar = []
-        self.outputGroupCheckbox = []
-        for i in range(9):
-            self.groupCheckboxVar.append(BooleanVar(value=False))
-            self.outputGroupCheckbox.append(Checkbutton(self.outputGroupFrame, text=i + 1, var=self.groupCheckboxVar[i], state=DISABLED))
-            self.outputGroupCheckbox[i].pack(side=LEFT)
-
-        # Button frame
-        self.buttonFrame = Frame(master)
-        self.buttonFrame.pack(side=BOTTOM)
-
-        # Execute button
-        self.executeButton = Button(self.buttonFrame, text="Execute", command=self.execute)
-        self.executeButton.pack(side=LEFT)
-
-        # Exit button
-        self.exitButton = Button(self.buttonFrame, text="Close", command=master.quit)
-        self.exitButton.pack(side=LEFT)
-
-    def execute(self):
-
-        input_text = self.inputText.get('1.0', 'end-1c')
-        regex_text = self.regexText.get('1.0', 'end-1c')
-
-        regex_flag = 0
-        if self.flagMultilineVar.get() is True:
-            regex_flag |= re.MULTILINE
-        if self.flagIgnoreCaseVar.get() is True:
-            regex_flag |= re.IGNORECASE
-        if self.flagDotAllVar.get() is True:
-            regex_flag |= re.DOTALL
-        if self.flagUnicodeVar.get() is True:
-            regex_flag |= re.UNICODE
-
-        try:
-            result = re.search(regex_text, input_text, regex_flag)
-            self.outputText.delete(1.0, END)
-            if result:
-                self.outputText.insert(END, result.group(0))
-                self.outputText.insert(END, "\n")
-
-                for i in range(9):
-                    try:
-                        group_text = result.group(i+1)
-                        self.outputGroupCheckbox[i].configure(state='normal')
-                        if self.groupCheckboxVar[i].get():
-                            self.outputText.insert(END, i+1)
-                            self.outputText.insert(END, ": [")
-                            self.outputText.insert(END, group_text)
-                            self.outputText.insert(END, "]\n")
-
-#                           Colorize group match
-#                           self.outputText.tag_add('group', '1.5', '1.10')
-#                           self.outputText.tag_add('group', '1.'.str(result.start(i+1)), '1.'.(result.end(i + 1)))
-#                           self.outputText.tag_config('group', background='yellow', foreground='red')
-
-                    except IndexError:
-                        self.outputGroupCheckbox[i].configure(state='disabled')
-                        self.groupCheckboxVar[i].set(False)
-            else:
-                for i in range(9):
-                    self.outputGroupCheckbox[i].configure(state='disabled')
-
-        except Exception as error:
-            self.outputText.delete(1.0, END)
-            self.outputText.insert(END, 'Error: '+error.message)
-
-    # Flag function
-    def getcheckbox(self, parent, name):
-        var = BooleanVar(value=False)
-        box = Checkbutton(parent, text=name, var=var)
-        box.pack(side=LEFT)
-        return box, var
+    except Exception as error:
+        outputText.delete(1.0, END)
+        outputText.insert(END, 'Error: '+error.message)
 
 
+# Flag function
+def getcheckbox(parent, name):
+    var = BooleanVar(value=False)
+    box = Checkbutton(parent, text=name, var=var)
+    box.pack(side=LEFT)
+    return box, var
 
 
-root = Tk()
-my_gui = RegexTestGUI(root)
+def button1callback(_):
+    execute()
 
-root.update()
-root.minsize(root.winfo_width(), root.winfo_height())
 
-root.mainloop()
+if __name__ == '__main__':
+
+    root = Tk()
+
+    # Input frame
+    inputFrame = Frame(root)
+    inputFrame.pack(side=TOP, fill=BOTH, expand=YES)
+
+    # Input text
+    inputText = Text(inputFrame, height=5, width=60)
+    inputText.insert(END, "Insert test text")
+    inputText.pack(side=LEFT, fill=BOTH, expand=YES)
+
+    # Input scrollbar
+    inputScroll = Scrollbar(inputFrame, command=inputText.yview)
+    inputText.configure(yscrollcommand=inputScroll.set)
+    inputScroll.pack(side=RIGHT, fill=Y)
+
+    # Regex frame
+    regexFrame = Frame(root)
+    regexFrame.pack(fill=X)
+
+    # Regex text
+    regexText = Text(regexFrame, height=3, width=60)
+    regexText.insert(END, "")
+    regexText.pack(side=LEFT, fill=X, expand=YES)
+
+    # Regex scrollbar
+    regexScroll = Scrollbar(regexFrame, command=regexText.yview)
+    regexText.configure(yscrollcommand=regexScroll.set)
+    regexScroll.pack(side=RIGHT, fill=Y)
+
+    # Flag frame
+    regexFlagFrame = Frame(root)
+    regexFlagFrame.pack(anchor=W)
+
+    # Flags
+    flagMultiline, flagMultilineVar = getcheckbox(regexFlagFrame, "Multiline")
+    flagIgnoreCase, flagIgnoreCaseVar = getcheckbox(regexFlagFrame, "IgnoreCase")
+    flagDotAll, flagDotAllVar = getcheckbox(regexFlagFrame, "DotAll")
+    flagUnicode, flagUnicodeVar = getcheckbox(regexFlagFrame, "Unicode")
+
+    # Output frame
+    outputFrame = Frame(root)
+    outputFrame.pack(fill=BOTH, expand=YES)
+
+    # Output text
+    outputText = Text(outputFrame, height=5, width=60)
+    outputText.insert(END, "")
+    outputText.pack(side=LEFT, fill=BOTH, expand=YES)
+    outputText.bind("<Key>", lambda e: "break")
+
+    # Output scrollbar
+    outputScroll = Scrollbar(outputFrame, command=outputText.yview)
+    outputText.configure(yscrollcommand=outputScroll.set)
+    outputScroll.pack(side=RIGHT, fill=Y)
+
+    # Group frame
+    outputGroupFrame = Frame(root)
+    outputGroupFrame.pack(anchor=W)
+
+    # Group checkboxes
+    groupCheckboxVar = []
+    outputGroupCheckbox = []
+    for index in range(9):
+        groupCheckboxVar.append(BooleanVar(value=False))
+        outputGroupCheckbox.append(Checkbutton(outputGroupFrame,
+                                               text=index + 1,
+                                               var=groupCheckboxVar[index],
+                                               state=DISABLED))
+
+        outputGroupCheckbox[index].pack(side=LEFT)
+
+    # Button frame
+    buttonFrame = Frame(root)
+    buttonFrame.pack(side=BOTTOM)
+
+    # Execute button
+    executeButton = Button(buttonFrame, text="Execute", command=execute)
+    executeButton.pack(side=LEFT)
+
+    # Exit button
+    exitButton = Button(buttonFrame, text="Close", command=root.quit)
+    exitButton.pack(side=LEFT)
+
+    root.title("RegexTest")
+
+    root.bind('<Button-1>', button1callback)
+    root.bind('<ButtonRelease-1>', button1callback)
+    root.bind('<Key>', button1callback)
+
+    root.update()
+    root.minsize(root.winfo_width(), root.winfo_height())
+
+    root.mainloop()
+
+    # Tests
+    # (\w+) (\w+) TEXT
+    # (\w+)\s(\w+)\s(\w+)\s(\w+)\s(\w+)\s(\w+)\s(\w+)\s(\w+)\s(\w+)
+    #
+'''
+Insert test text asdadfsdfadsf asdfasdf sdfadsfasf
+sadfasdfasdf asdf dfasdfas dfasd fasdf asdfas dfas
+fsd ggasdgr ergasd fdg sefsg fg fgsg f gasdasdgsdg
+'''
